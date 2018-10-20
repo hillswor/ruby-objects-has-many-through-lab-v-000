c5fe3d0cf39a272eb113bedfbe2e362e5c7f530f
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2018-10-20 22:10:46 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -16,15 +16,14 @@ class Genre
     end
   end
 
-  def self.all 
-    @@all 
+  def self.all
+    @@all
   end
 
-  def artists 
+  def artists
     self.songs.collect do |song|
       song.artist
     end
   end
-      
 
 end
