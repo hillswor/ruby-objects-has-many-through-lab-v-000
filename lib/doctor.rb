26584d51446c2ee30370d6277fbf5c677e6a89e2
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2018-10-20 22:20:50 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -17,11 +17,11 @@ class Doctor
     Appointment.new(patient, self, date)
   end
 
-  def appointments 
+  def appointments
     Appointment.all.select do |appointment|
-      appointment.doctor == self 
+      appointment.doctor == self
     end
   end
-    
+
 
 end
